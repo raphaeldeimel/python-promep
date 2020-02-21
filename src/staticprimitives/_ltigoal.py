5d1cc4afb5a5aa0243acbdca8ac06e662766b74e
raphael.deimel <raphael.deimel@tu-berlin.de>
2020-02-21 11:25:09 +0100

    update litgoals to namedtensors

--- src/staticprimitives/_ltigoal.py
+++ src/staticprimitives/_ltigoal.py
@@ -22,6 +22,7 @@ import matplotlib.pyplot as _plt
 
 import mechanicalstate as _mechanicalstate
 
+import namedtensors as _nt
 
 class LTIGoal(object):
     """
@@ -31,148 +32,167 @@ class LTIGoal(object):
     
     """
 
-    def __init__(self, task_space='jointspace', kp=10.0, kv=5.0, kd=0.0, name='unnamed', mechanicalstate=None, dofs=None, desiredPosition=None, desiredTorque=None, desiredVelocity=None, expectedTorqueNoise=0.01 , **kwargs):
+    def __init__(self, tensornamespace, * , current_msd_from=None, task_space='jointspace', kp=10.0, kv=5.0, kd=0.0, desiredTensorData=None, expectedTorqueNoise=0.01 , name='unnamed'):
+
         self.name = name
         self.phaseAssociable = False #indicate that this motion generator is not parameterized by phase
         self.timeAssociable = True #indicate that this motion generator is parameterizable by time
         self.taskspace_name = task_space
         
-        self.tns = _nt.TensorNameSpace()
-        if mechanicalstate is not None:            
-            [ self.registerIndex(iname,mechanicalstate.index_sizes[iname]) for iname in mechanicalstate.index_sizes ]
-        else: #assume default sizes:
+        if not tensornamespace is None:
+            self.tns = _nt.TensorNameSpace(tensornamespace) #inherit index sizes
+            if self.tns.indexSizes['r'] < 2:
+                raise NotImplementedError() #sorry, this "trivial" functionality is not implemented. Try using a simple, fixed msd instead
+        else:
+            self.tns = _nt.TensorNameSpace()
             self.tns.registerIndex('r',2)
             self.tns.registerIndex('g',2)
-            self.tns.registerIndex('d',2)
-        self.name2rg = mechanicalstate.name2rg
-        
-        ones = _np.ones(self.tns.indexSizes['d'])
-        kp = ones * _np.asarray(kp)
-        kd = ones * _np.asarray(kd)
-        kv = ones * _np.asarray(kv)
+            self.tns.registerIndex('d',1)
 
-                
-        self.tns.registerTensor('desired', (('r','g','d'),()) )
-        if desiredPosition is not None:
-           self.tns.tensorData['desired'][self.name2rg['position']][:] = desiredPosition
-        if desiredVelocity is not None:
-           self.tns.tensorData['desired'][self.name2rg['velocity']][:] = desiredVelocity
-        if desiredTorque is not None:
-           self.tns.tensorData['desired'][self.name2rg['torque']][:] = desiredTorque
-        
-        self.tns.registerTensor('U', (('r2','g2','d2'),('r','g','d')) )
-        self.tns.registerTensor('K', (('r2','g2','d2'),('r','g','d')) )
+        self.tns.registerIndex('r2',self.tns.indexSizes['r'], self.tns.indexValues['r'])
+        self.tns.registerIndex('g2',self.tns.indexSizes['g'])
+        self.tns.registerIndex('d2',self.tns.indexSizes['d'],)
 
-        self.tns.registerTensor('cov', (('r','g','d'),('r_','g_','d_')) )
-        self.tns.registerTensor('mean', (('r','g','d'),()) )
-        self.tns.registerTensor('noise', (('r2','g2','d2'),('r2_','g2_','d2_')) )
-        tautau  = self.name2rg['torque'] +(None) + self.name2rg['torque'] + (None)
-        self.tns.tensorData['noise'][tautau] = _np.asarray(expectedTorqueNoise)
+            
+        self.msd_desired = _mechanicalstate.MechanicalStateDistribution(self.tns, "DesiredMean", "DesiredCov")
+        self.msd_expected = _mechanicalstate.MechanicalStateDistribution(self.tns, "ExpectedMean", "ExpectedCov")
+        self.commonnames2rg = self.msd_desired.commonnames2rg
+
+        if current_msd_from is None: #local copy
+            self.tns.registerTensor('CurrentMean', (('r','g','d'),()) )
+            self.tns.registerTensor('CurrentCov', (('r','g','d'),('r_', 'g_', 'd_')) )
+            self.msd_current = _mechanicalstate.MechanicalStateDistribution(self.tns, "CurrentMean", "CurrentCov")
+        else:   #use data from somewhere else:
+            self.tns.registerTensor('CurrentMean', current_msd_from.tns.tensorIndices[current_msd_from.meansName] , external_array=current_msd_from.tns.tensorData[current_msd_from.meansName], initial_values='keep' )
+            self.tns.registerTensor('CurrentCov', current_msd_from.tns.tensorIndices[current_msd_from.covariancesName] , external_array=current_msd_from.tns.tensorData[current_msd_from.covariancesName], initial_values='keep')
+            self.msd_current = current_msd_from
+
+
+        #desired values:
+        self.tns.registerTensor('DesiredMean', (('r','g','d'),()) )
+        self.tns.registerTensor('DesiredCov', (('r','g','d'),('r_', 'g_', 'd_')) )
+
+
+        r_torque, g_torque = self.commonnames2rg['torque']
+        r_pos, g_pos = self.commonnames2rg['position']  
+        self.tns.registerBasisTensor('e_ktau',  (('r2', 'g2'),('r', 'g')), (('effort', g_torque),('effort', g_torque)) )
+        self.tns.registerTensor('Uktau',  (('d2',),('d',)), initial_values='identity')
+        self.tns.registerBasisTensor('e_kp',  (('r2', 'g2'),('r', 'g')), (('effort', g_torque),('motion', g_pos)))
+        self.tns.registerTensor('Kp', (('d2',),('d',)) )
+        
+        
+        slice_tau = self.tns.registerContraction('e_ktau', 'Uktau')
+        slice_kp = self.tns.registerContraction('e_kp', 'Kp')
+        if 'velocity' in self.commonnames2rg:
+            r_vel, g_vel = self.commonnames2rg['velocity']  
+            self.tns.registerBasisTensor('e_kv',  (('r2', 'g2'),('r', 'g')), (('effort', g_torque),('motion', g_vel)))
+            self.tns.registerTensor('Kv', (('d2',),('d',)) )
+            slice_kv = self.tns.registerContraction('e_kv', 'Kv' )
+
+            #add together:
+            self.tns.registerSum(slice_tau, slice_kp, slice_kv, result_name='U')
+        else:
+            self.tns.registerAddition(slice_tau, slice_kp, result_name='U') 
+
+        self.tns.registerTensor('I', self.tns.tensorIndices['U'], initial_values='identity')
+
+        if 'velocity' in self.commonnames2rg:  #can we add damping terms? (kd)
+            #Compute the K tensor: 
+            self.tns.registerTensor('Kd', (('d2',),('d',)) )  #kd is equal to kv but with goal velocity=0  -> only appears here and not in the computation of 'U'
+            slice_kd = self.tns.registerContraction('e_kv', 'Kd') 
+            U_plus_damping = self.tns.registerAddition(slice_kd, 'U')
+            
+            self.tns.registerSubtraction('I', U_plus_damping, result_name='K')
+        else:
+            self.tns.registerSubtraction('I', 'U', result_name = 'K')
 
         self.tns.registerTranspose('U')
         self.tns.registerTranspose('K')
-        self.tns.registerContraction('U', 'desired')
-        
-        
-        desiredU = _t.dot( self.U, self.desired ) 
 
-        self.registerContraction('K', 'cov')
-        self.registerContraction('K:cov', '(K)^T')
-        self.registerAddition('K:cov:(K)^T', 'noise', result_name='covExpected')
-        self.registerContraction('K', 'mean')
-        self.registerAddition('K:mean', 'U:desired', result_name='meanExpected')
         
-        self.operations_every_time = self.tns.update_order[self.tns.update_order.index('K:cov'),:]
+        self.tns.registerTensor('noise', (('r','d','d'),('r_','g_','d_')) )
 
-        self._updateKU(newKp=kp, newKv=kv, newKd=kd)
         
+        #influence of desired mean on expected mean:
+        term_meanU = self.tns.registerContraction('U', 'DesiredMean')
+        
+        #influence of desired cov on expected cov:
+        previous = self.tns.registerContraction('U', 'DesiredCov')
+        term_covU = self.tns.registerContraction(previous, '(U)^T')
+
+        #up to here we can precompute the equations if goals don't change
+        self._update_cheap_start = len(self.tns.update_order)
 
 
-    def _updateKU(self, newKp=None, newKv=None, newKd=None):
+        
+        #influence of current cov to expected cov:
+        previous = self.tns.registerContraction('K', 'CurrentCov')
+        term_covK = self.tns.registerContraction(previous, '(K)^T')
+        self.tns.registerAddition(term_covK, term_covU, result_name='ExpectedCov')
+
+        #influence of current mean on expected mean:        
+        previous = self.tns.registerContraction('K', 'CurrentMean')
+        self.tns.registerAddition(previous, term_meanU, result_name='ExpectedMean')
+
+        droptwos={'r2':'r', 'g2':'g', 'd2':'d', 'r2_':'r_', 'g2_':'g_', 'd2_':'d_'}
+        self.tns.renameIndices('ExpectedCov', droptwos, inPlace=True)
+        self.tns.renameIndices('ExpectedMean', droptwos, inPlace=True)
+
+
+        #set values, if provided:
+        self.setDesired(desiredMean=desiredTensorData, Kp=kp, Kv=kv)
+        self.tns.update(*self.tns.update_order[:self._update_cheap_start])
+
+
+    def setDesired(self, desiredMean=None, desiredCov=None, Kp=None, Kv=None):
         """
-        update the state evolution matrices according to the current mass and (optionally) timestep duration
-        """
-        #first, save values and decide on what to recompute
-        updateAll=False
-        if newKp is not None:
-            self.kp = _np.asarray(newKp)
-            updateAll = True
-        if newKv is not None:
-            self.kv = _np.asarray(newKv)
-            updateAll = True
-        if newKd is not None:
-            self.kd = _np.asarray(newKd)
-            updateAll = True
-        
-        
-        #recompute the mass-free update matrices:
-        if updateAll:
-            for dof in range(self.tns.indexSizes['d']):
-                self.tensorData['U'][:,:,dof,:,:,dof] = [
-                    [1, self.kp[dof], self.kv[dof]],
-                    [0,0,0],
-                    [0,0,0],
-                ]
-                self.tensorData['K'][:,:,dof,:,:,dof] = [
-                    [0, -self.kp[dof], (-self.kv[dof]-self.kd[dof])],
-                    [0,1,0],
-                    [0,0,1],
-                ]
-            self.tns.update(('U:desired'))
-
-            
-#    def setDesiredPosition(self, desiredPosition):
-#        """
-#        set a new desired position
-#        """
-#        
-#        self.tns.setTensor('desired'], 0.0)
-#        self.tns.tensorData['desired'][self.name2rg['position']][:] = desiredPosition
-#        self.tns.update(('U:desired'))
-
-#    def setDesiredTorque(self, desiredTorque):
-#        """
-#        set a new desired position
-#        """
-#        self.tns.tensorData['desired'][self.name2rg['torque']][:] = desiredTorque
-#        self.tns.update(('U:desired'))
-
-
-    def setDesired(self, desiredMean, expectedTorqueNoise):
-        """updateDesired
         set new desired means and covariances and recompute any dependent internal variables
-        """
-        self.setTensor('desired', desiredMean)
-        self.tns.update(('U:desired'))
         
-        tautau  = self.name2rg['torque'] +(None) + self.name2rg['torque'] + (None)
-        self.tns.tensorData['noise'][tautau] = _np.asarray(expectedTorqueNoise)
-                
+        desiredMean: array of shape ('r', 'g', 'd')
 
+        desiredCov: array of shape ('r', 'g', 'd', r_, g_, d_)
 
-    def getDistribution(self, generalized_phase=None, current_msd=None, task_spaces=None):
+       
+        Kp: array of shape ('d', 'd_')    (order: torque, position)
+
+        Kv: array of shape ('d', 'd_')    (order: torque, velocity)
         """
-        modify current_msd so it obeys the pd-control law
-            (i.e. changes the torque-related means and covariances)
-        
-           phase: not used (ignored)
+        if self.msd_current.tns is self.tns:  #local data?
+            if not desiredMean is None:            
+                self.tns.setTensor('DesiredMean', desiredMean)
+            if not desiredCov is None:
+                self.tns.setTensor('DesiredCov', desiredCov)
+        else:
+            if not (desiredMean is None and desiredCov is None):
+                raise ValueError("I don't dare setting a data array I don't own.")
+            
+        for value, tensorname in ( (Kp, 'Kp'), (Kv, 'Kv')):
+            if value is None:
+                continue
+            if _np.isreal(value):
+                pass
+            elif value.ndim == 1:
+                Kp = _np.diag(Kp)
+            else:
+                pass
+            self.tns.setTensor(tensorname, value)
+        self.tns.update(*self.tns.update_order[:self._update_cheap_start])
+            
 
-           phaseVelocity: not used (ignored)
 
-           currentDistribution: (means, covariances) tuple of multivariate normal distribution
-           
-           phaseVelocitySigma: not used (ignored)
-
-           returns: (means, covariances) tuple
-               means: (derivatives x dofs) array
-               covariances: (derivatives x dofs x derivatives x dofs) array
+    def getDistribution(self, *, current_msd=None, task_spaces=None, **kwargs):
         """
-        self.tns.setTensor('mean', current_msd.means)
-        self.tns.setTensor('cov', current_msd.covariances)
-
-        self.tns.update(self.operations_every_time)
+            return an expected mechanicalstate distribution 
+            constructed from the current msd and the lti goals
         
-        return MechanicalStateDistribution(self.tns.tensorData['mean'], self.tns.tensorData['cov'])
+        """
+        if not current_msd is None:
+            self.tns.setTensor('CurrentMean', current_msd.getMeansData())
+            self.tns.setTensor('CurrentCov', current_msd.getCovariancesData())
+
+        self.tns.update(*self.tns.update_order[self._update_cheap_start:])
+        
+        return self.msd_expected
     
     
     
@@ -228,8 +248,17 @@ class LTIGoal(object):
         _h5.write(d, filename=filepath, store_python_metadata=True)
         return filepath
 
-
-
+        
+    def __repr__(self):
+        text  = "Name: {}\n".format(self.name)
+        text += "r,g,d: {},{},{}\n".format(self.tns.indexSizes['r'],self.tns.indexSizes['g'], self.tns.indexSizes['d'] )
+        for name in ('Kp', 'Kv', 'Kd'):            
+            if name in self.tns.tensorData:
+                text += "{}:\n".format(name)
+                text += "{}\n".format(self.tns.tensorData[name])
+        return text
+    
+    
 
     def plot(self, dofs='all',
                    distInitial = None,
@@ -269,8 +298,8 @@ class LTIGoal(object):
         tns_plot.registerTensor('cov_sampled', (('r','g','d'),('r_','g_','d_')))
         if distInitial is None:
             #create a useful initial dist for plotting:
-            self.tensorData['mean_initial'][self.name2rg['position']][:] = 1.4
-            self.tensorData['mean_initial'][self.name2rg['position']][:] = 0.0
+            self.tensorData['mean_initial'][self.commonnames2rg['position']][:] = 1.4
+            self.tensorData['mean_initial'][self.commonnames2rg['position']][:] = 0.0
         else:
             self.setTensor('mean_initial', distInitial.means)
             self.setTensor('cov_initial', distInitial.covariances)
@@ -282,13 +311,13 @@ class LTIGoal(object):
             'velocity': [(a+b)-1*(b-a), (a+b) + 1*(b-a) ],
         }
         limits={}
-        for limitname in self.name2rg:
+        for limitname in self.commonnames2rg:
             limits[limitname] = limits_tightest[limitname]
 
         if dofs_to_plot=='all':
             dofs_to_plot=list(range(self.tns.indexSizes['d']))
 
-        mStateNames = list(self.tns.names2rg)
+        mStateNames = list(self.tns.commonnames2rg)
         mstates = len(mStateNames)
 
         #if no mass matrix inverse is provided, assume a decoupled unit mass system for plotting
@@ -351,14 +380,14 @@ class LTIGoal(object):
             for limitname in limits:
                 for m in range(data_mean.shape[1]):
                     mstateIndex = self._md.mStateNames2Index[limitname]
-                    sigma =tns_plot.tensorData['data_sigma'][self.names2rg[limitname]]
-                    mean =tns_plot.tensorData['data_mean'][self.names2rg[limitname]]
+                    sigma =tns_plot.tensorData['data_sigma'][self.commonnames2rg[limitname]]
+                    mean =tns_plot.tensorData['data_mean'][self.commonnames2rg[limitname]]
                     limits[limitname][0] = min(_np.min(mean-1.96*sigma), limits[limitname][0])
                     limits[limitname][1] = max(_np.max(mean+1.96*sigma), limits[limitname][1])
             #plot all dofs
             for d, dof in enumerate(dofs_to_plot):
                 for j, name in enumerate(mStateNames):
-                    slicedef  = (None) + self.name2rg[name] + (dof)
+                    slicedef  = (None) + self.commonnames2rg[name] + (dof)
                     axesArray[j,d].plot(t, tns_plot.tensorData['data_mean'][slicedef], alpha=alpha, linewidth=linewidth )
 
         #override scaling:
